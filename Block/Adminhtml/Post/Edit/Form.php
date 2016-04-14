bfbe5d5a94aa191022befbf1736c6e63dfe27dec
Zilus <neozilus@gmail.com>
2016-04-14 16:02:43 -0500

    First commit
    Base dev

---
+++ Block/Adminhtml/Post/Edit/Form.php
@@ -0,0 +1,124 @@
+<?php
+namespace TCK\Blog\Block\Adminhtml\Post\Edit;
+
+/**
+ * Adminhtml blog post edit form
+ */
+class Form extends \Magento\Backend\Block\Widget\Form\Generic
+{
+
+    /**
+     * @var \Magento\Store\Model\System\Store
+     */
+    protected $_systemStore;
+
+    /**
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\Data\FormFactory $formFactory
+     * @param \Magento\Cms\Model\Wysiwyg\Config $wysiwygConfig
+     * @param \Magento\Store\Model\System\Store $systemStore
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Data\FormFactory $formFactory,
+        \Magento\Store\Model\System\Store $systemStore,
+        array $data = []
+    ) {
+        $this->_systemStore = $systemStore;
+        parent::__construct($context, $registry, $formFactory, $data);
+    }
+
+    /**
+     * Init form
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setId('post_form');
+        $this->setTitle(__('Información de la entrada'));
+    }
+
+    /**
+     * Prepare form
+     *
+     * @return $this
+     */
+    protected function _prepareForm()
+    {
+        /** @var \TCK\Blog\Model\Post $model */
+        $model = $this->_coreRegistry->registry('blog_post');
+
+        /** @var \Magento\Framework\Data\Form $form */
+        $form = $this->_formFactory->create(
+            ['data' => ['id' => 'edit_form', 'action' => $this->getData('action'), 'method' => 'post']]
+        );
+
+        $form->setHtmlIdPrefix('post_');
+
+        $fieldset = $form->addFieldset(
+            'base_fieldset',
+            ['legend' => __('Información'), 'class' => 'fieldset-wide']
+        );
+
+        if ($model->getPostId()) {
+            $fieldset->addField('post_id', 'hidden', ['name' => 'post_id']);
+        }
+
+        $fieldset->addField(
+            'title',
+            'text',
+            ['name' => 'title', 'label' => __('Título'), 'title' => __('Título'), 'required' => true]
+        );
+
+        $fieldset->addField(
+            'url_key',
+            'text',
+            [
+                'name' => 'url_key',
+                'label' => __('URL Key'),
+                'title' => __('URL Key'),
+                'required' => true,
+                'class' => 'validate-xml-identifier'
+            ]
+        );
+
+        $fieldset->addField(
+            'is_active',
+            'select',
+            [
+                'label' => __('Estatus'),
+                'title' => __('Estatus'),
+                'name' => 'is_active',
+                'required' => true,
+                'options' => ['1' => __('Habilitada'), '0' => __('Deshabilitada')]
+            ]
+        );
+        if (!$model->getId()) {
+            $model->setData('is_active', '1');
+        }
+
+        $fieldset->addField(
+            'content',
+            'editor',
+            [
+                'name' => 'content',
+                'label' => __('Contenido'),
+                'title' => __('Contenido'),
+                'style' => 'height:36em',
+                'wysiwyg'   => true,
+                'required' => true
+            ]
+        );
+
+        $form->setValues($model->getData());
+        $form->setUseContainer(true);
+        $this->setForm($form);
+
+        return parent::_prepareForm();
+    }
+}
